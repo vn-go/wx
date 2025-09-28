9cacc19c3f85c754364f60260bb15fa04071add8
Nttlong <zugeliang2000@gmail.com>
2025-09-28 15:30:51 +0700

    support body validate tags

--- validators_test.go
+++ validators_test.go
@@ -2,37 +2,38 @@ package wx
 
 import (
 	"testing"
-	"time"
 )
 
 // ---- Test struct ----
 type DataTest struct {
-	Name        string    `check:"range:(10:20);"`
-	Email       string    `check:"regex:^.+@.+\\..+$;range:(40,);"`
-	Date        time.Time `check:"range:(2010-10-11 12:23:44Z,2100-10-11)"`
-	Description string    `check:"range:(20:)"`
-	Salary      *float64  `check:"range:[1:)"`
-	Code        *string
+	Name string `json:"code" check:"range:[3:8]"`
+	// Email       string    `check:"regex:^.+@.+\\..+$;range:(40,);"`
+	// Date        time.Time `check:"range:(2010-10-11 12:23:44Z,2100-10-11)"`
+	// Description string    `check:"range:(20:)"`
+	// Salary      *float64  `check:"range:[1:)"`
+	// Code        *string
 }
 
 func TestXxx(t *testing.T) {
-	f := -1.77
+	// f := -1.77
 	ret := Validators.Check(&DataTest{
-		Salary: &f,
-		Email:  "test@gmail.com",
-		Date:   time.Date(2010, 10, 11, 12, 23, 44, 2, time.UTC), //sao van kg hop le
+		Name: "ADM",
+		// Salary: &f,
+		// Email:  "test@gmail.com",
+		// Date:   time.Date(2010, 10, 11, 12, 23, 44, 2, time.UTC), //sao van kg hop le
 	})
 	t.Log(ret)
 
 }
-func BenchmarkXxx(b *testing.B) {
-	f := -1.77
-	for i := 0; i < b.N; i++ {
-		Validators.Check(&DataTest{
-			Salary: &f,
-			Email:  "test@gmail.com",
-			Date:   time.Date(2010, 10, 11, 12, 23, 44, 2, time.UTC), //sao van kg hop le
-		})
 
-	}
-}
+// func BenchmarkXxx(b *testing.B) {
+// 	f := -1.77
+// 	for i := 0; i < b.N; i++ {
+// 		Validators.Check(&DataTest{
+// 			Salary: &f,
+// 			Email:  "test@gmail.com",
+// 			Date:   time.Date(2010, 10, 11, 12, 23, 44, 2, time.UTC), //sao van kg hop le
+// 		})
+
+// 	}
+// }
